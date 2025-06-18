c3c3dd04ba2decb40a94f17a42c754bab1fc05fe
Murilo Marinho <murilomarinho@manchester.ac.uk>
2025-06-18 11:14:40 +0100

    Adding -y flag

--- .devel/prebuild_ros2.sh
+++ .devel/prebuild_ros2.sh
@@ -2,4 +2,4 @@
 # Copyright (c) Murilo M. Marinho (www.murilomarinho.info)
 
 # Pre-requisites
-sudo apt-get install --y dh-make dh-python python3-bloom doxygen
+sudo apt-get install -y dh-make dh-python python3-bloom doxygen
