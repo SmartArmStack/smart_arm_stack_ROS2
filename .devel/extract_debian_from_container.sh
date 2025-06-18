571663dd415d3416099668d781208a645df727fa
Murilo Marinho <murilomarinho@manchester.ac.uk>
2025-06-18 15:02:37 +0100

    Extracting debian files and uploading artificat

--- .devel/extract_debian_from_container.sh
+++ .devel/extract_debian_from_container.sh
@@ -4,6 +4,6 @@ set -e
 
 mkdir -p upload
 
-docker run -d --name extract_files sas_deb_builder || true
+docker run -t -d --name extract_files sas_deb_builder || true
 docker exec extract_files bash -c "mkdir -p ~/extract; cp -f /root/tmp_ros2/*.deb ~/extract"
 docker cp extract_files:/root/extract upload/
