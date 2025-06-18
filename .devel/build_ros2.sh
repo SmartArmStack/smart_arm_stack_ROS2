ca1d9559e2f07ef04128074158a3b24fb915064c
Murilo Marinho <murilomarinho@manchester.ac.uk>
2025-06-18 14:37:36 +0100

    Maybe arch is not needed

--- .devel/build_ros2.sh
+++ .devel/build_ros2.sh
@@ -13,13 +13,6 @@ export DEB_BUILD_OPTIONS=parallel=4
 rosv="jazzy"
 # Ubuntu version
 ubuntuv="noble"
-# Architecture
-arch=$(uname -i)
-if [[ $arch == x86_64* ]]; then
-    archv="amd64"
-elif  [[ $arch == arm* ]]; then
-    archv="arm64"
-fi
 
 PRE_BUILD() {
 # Remove the debian folder just in case. In ROS1 and catkin this was an issue
@@ -126,6 +119,6 @@ for pkg_name in "${pkg_array[@]}"; do
   cd ..
   # Install package but replace _ by -. E.g. sas_core becomes sas-core.
   # https://stackoverflow.com/questions/3306007/replace-a-string-in-shell-script-using-a-variable
-  sudo dpkg -i ros-"$rosv"-"${pkg_name//_/-}"_*"$ubuntuv"_"$archv".deb
+  sudo dpkg -i ros-"$rosv"-"${pkg_name//_/-}"_*"$ubuntuv"*.deb
   ${var//12345678/$replace}
 done
