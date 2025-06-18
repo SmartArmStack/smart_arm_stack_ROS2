4598ae5adbe781396834108ede29692e3808beee
Murilo Marinho <murilomarinho@manchester.ac.uk>
2025-06-18 14:09:19 +0100

    Adding the clone instructions where it needs to be.

--- .devel/prebuild_ros2.sh
+++ .devel/prebuild_ros2.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 # Copyright (c) Murilo M. Marinho (www.murilomarinho.info)
 
-# SAS-RELATED THINGS (no need for recursive here)
-mkdir -p ~/sas_ws/src
-cd ~/sas_ws/src || exit 1
-git clone -b jazzy https://github.com/SmartArmStack/smart_arm_stack_ROS2.git sas
-
 # Pre-requisites
 sudo apt-get update
-sudo apt-get install -y dh-make dh-python python3-bloom doxygen
-sudo apt-get install --only-upgrade g++ cmake git python3-pybind11
+sudo apt-get install -y dh-make dh-python python3-bloom
