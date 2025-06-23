da98258da3cf5fa86a982b321bca960f0ef63f79
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2025-06-23 21:20:01 +0100

    Update extract_debian_from_container.sh

--- .devel/extract_debian_from_container.sh
+++ .devel/extract_debian_from_container.sh
@@ -5,6 +5,5 @@ set -e
 mkdir -p upload
 
 docker run -t -d --name extract_files sas_deb_builder || true
-docker exec extract_files bash -c "mkdir -p ~/extract; cp -f /root/tmp_ros2/*.deb ~/extract"
-docker exec extract_files bash -c "mkdir -p ~/extract; cp -f /root/tmp_ros2/SAS_VERSION ~/extract"
+docker exec extract_files bash -c "mkdir -p ~/extract; cp -f /root/tmp_ros2/*.deb ~/extract; cp -f /root/tmp_ros2/SAS_VERSION ~/extract"
 docker cp extract_files:/root/extract upload/
