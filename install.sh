9a540208f6c7c372f79839a3dbb021af77030c28
Murilo Marinho <murilomarinho@manchester.ac.uk>
2025-06-23 16:28:36 +0100

    Adding docker file

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
-# Make a temporary directory
-mkdir -p smart_arm_stack_ros2_installation
-cd smart_arm_stack_ros2_installation
-# Remove existing packages, if any, and don't complain if there are none
-sudo apt remove ros-humble-sas* -y || true
-# Download files
-wget $(curl -sL https://api.github.com/repos/smartarmstack/smart_arm_stack_ROS2/releases/latest | jq -r '.assets[].browser_download_url')
-# Install packages
-sudo dpkg -i  ros-humble-sas-*.deb
-# Remove temporary folder and itself
-cd ..
-rm -r smart_arm_stack_ros2_installation
-rm -- "$0"
+#!/bin/bash
+curl -s --compressed "https://smartarmstack.github.io/smart_arm_stack_ROS2/KEY.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/smartarmstack_lgpl.gpg >/dev/null
+sudo curl -s --compressed -o /etc/apt/sources.list.d/smartarmstack_lgpl.list "https://smartarmstack.github.io/smart_arm_stack_ROS2/smartarmstack_lgpl.list"
+sudo apt update
+sudo apt-get install ros-jazzy-sas-*
