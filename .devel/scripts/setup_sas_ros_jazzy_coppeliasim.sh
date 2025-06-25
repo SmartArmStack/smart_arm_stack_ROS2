ed40da82b38a288f6ea8be258cd0757b2415e147
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2025-06-25 21:50:19 +0100

    [setup_sas_ros_jazzy_coppeliasim.sh] Updating with /Users/murilomarinho because ~ does not expand as expected.

--- .devel/scripts/setup_sas_ros_jazzy_coppeliasim.sh
+++ .devel/scripts/setup_sas_ros_jazzy_coppeliasim.sh
@@ -22,7 +22,7 @@ https://downloads.coppeliarobotics.com/"${COPPELIASIM_RELEASE}"/"${COPPELIASIM_F
 
 tar -xf "${COPPELIASIM_FILE}"
 
-echo "export COPPELIASIM_PATH='~/utils/CoppeliaSim_Edu_${COPPELIASIM_RELEASE}_${UBUNTU_VERSION}'">> ~/.bashrc
+echo "export COPPELIASIM_PATH='${HOME}/utils/CoppeliaSim_Edu_${COPPELIASIM_RELEASE}_${UBUNTU_VERSION}'">> ~/.bashrc
 # shellcheck disable=SC2016
 # Suppressed because we want to use $COPPELIASIM_PATH in bashrc.
 echo 'alias coppeliasim=$COPPELIASIM_PATH/coppeliaSim.sh & '>> ~/.bashrc
