be1a30261940a2a9eebdd95e85074f9bc276551b
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2025-07-04 22:33:58 +0100

    Removing submodules to reduce confusion, removed coppeliasim commands.

--- .git_submodule_autocommit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for d in */ ; do
-    echo "Git commit in $d..."
-    cd "$d"
-    git commit -a -m "[Auto commit submodules]"
-    cd ..
-done
--- .git_submodule_status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for d in */ ; do
-    echo "Git status in $d..."
-    cd "$d"
-    git status
-    cd ..
-done
-
