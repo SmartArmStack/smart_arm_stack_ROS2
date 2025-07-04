27b68eead7e10fbd24d5672a3b1dfb3bf9437875
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2025-07-04 22:37:40 +0100

    [cleanup] Removing unused dev files

--- .devel/run_container.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-# Based on juanjqo's https://github.com/Adorno-Lab/docker_recipes/blob/main/run_container.sh
-
-docker run -it --name=$1 \
---rm --privileged --network=host --env=DISPLAY \
-sas_deb_builder /bin/bash
