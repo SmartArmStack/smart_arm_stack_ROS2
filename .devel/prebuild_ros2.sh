6605ea829e0373fd26320c6df7f84b9e16776757
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2025-11-04 13:52:23 +0000

    [prebuild_ros2.sh] Add upgrade and autoremove to apt.
    Added upgrade and autoremove commands to the script.

--- .devel/prebuild_ros2.sh
+++ .devel/prebuild_ros2.sh
@@ -3,5 +3,7 @@ set -e
 # Copyright (c) Murilo M. Marinho (www.murilomarinho.info)
 
 # Pre-requisites
-sudo apt-get update
+sudo apt-get update -q
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y
 sudo apt-get install -y dh-make dh-python python3-bloom
