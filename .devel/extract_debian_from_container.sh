31ce1adb2b7a6d698d72948df253b6bf1e280bce
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2026-04-20 18:20:17 +0100

    [sas_debian_builder] Trying to add debugsyms to PPA.

--- .devel/extract_debian_from_container.sh
+++ .devel/extract_debian_from_container.sh
@@ -6,4 +6,6 @@ mkdir -p upload
 
 docker run -t -d --name extract_files sas_deb_builder || true
 docker exec extract_files bash -c "mkdir -p ~/extract; cp -f /root/tmp_ros2/*.deb ~/extract; cp -f /root/tmp_ros2/SAS_VERSION ~/extract"
+# Debug symbol packages
+docker exec extract_files bash -c "mkdir -p ~/extract; cp -f /root/tmp_ros2/*.ddeb ~/extract; cp -f /root/tmp_ros2/SAS_VERSION ~/extract"
 docker cp extract_files:/root/extract upload/
