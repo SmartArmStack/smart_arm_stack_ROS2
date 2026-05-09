2a21ec3900c113836b44f9e28ed1ce531d2aac79
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2026-05-09 12:32:15 +0100

    [lyrical] Using correct installation.

--- lyrical/install_ros2.sh
+++ lyrical/install_ros2.sh
@@ -5,12 +5,11 @@ apt-get update
 apt-get install -y software-properties-common curl
 add-apt-repository universe
 
-apt-get update
+apt update
 export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F'"' '{print $4}')
-curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
-dpkg -i /tmp/ros2-apt-source.deb
+curl -L -o /tmp/ros2-testing-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-testing-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
+dpkg -i /tmp/ros2-testing-apt-source.deb
 
 apt-get update
 apt-get install -y ros-dev-tools
-apt-cache search ros-lyrical
 apt-get install -y ros-lyrical-desktop
