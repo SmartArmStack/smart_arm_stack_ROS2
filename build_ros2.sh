dabc0052ecae8651db77ca42833239b9379f0d37
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2026-05-16 18:51:46 +0100

    Added sas_robot_driver_kuka

--- build_ros2.sh
+++ build_ros2.sh
@@ -60,7 +60,7 @@ sas_pkg_array=(
 # Packages in https://github.com/MarinhoLab/
 marinholab_pkg_array=(
 "sas_robot_driver_coppeliasim"
-#"sas_robot_driver_kuka"
+"sas_robot_driver_kuka"
 #"sas_robot_driver_ur"
 #"sas_force_sensor_bota"
 )
