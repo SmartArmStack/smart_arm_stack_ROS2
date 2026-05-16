0a7ce0378547d54596ef58515c212fdfce9ab9e0
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2026-05-16 19:34:37 +0100

    [lyrical] Adding sas dockerfile.

--- extract_debian_from_container.sh
+++ extract_debian_from_container.sh
@@ -2,9 +2,12 @@
 # Copyright (c) Murilo M. Marinho (www.murilomarinho.info)
 set -e
 
+# ROS2 version
+rosv=$1
+
 mkdir -p upload
 
-docker run -t -d --name extract_files sas_deb_builder || true
+docker run -t -d --name extract_files "$rosv"/sas_deb_builder || true
 docker exec extract_files bash -c "mkdir -p ~/extract; cp -f /root/tmp_ros2/*.deb ~/extract; cp -f /root/tmp_ros2/SAS_VERSION ~/extract"
 # Debug symbol packages
 docker exec extract_files bash -c "mkdir -p ~/extract; cp -f /root/tmp_ros2/*.ddeb ~/extract; cp -f /root/tmp_ros2/SAS_VERSION ~/extract"
