22fa30656c5197495fe49a3409dfc19de68c71bd
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2026-05-21 16:58:03 +0100

    Update prebuild_ros2.sh

--- prebuild_ros2.sh
+++ prebuild_ros2.sh
@@ -7,3 +7,6 @@ sudo apt-get update -q
 sudo apt-get upgrade -y
 sudo apt-get autoremove -y
 sudo apt-get install -y dh-make dh-python python3-bloom
+
+# sas_robot_driver_gazebo
+sudo apt-get install libgz-transport13-dev
